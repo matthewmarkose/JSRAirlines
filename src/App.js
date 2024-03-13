d0f94485cd6d3edbadc5903452c7f309698b6249
Matthew Markose <matt.markose@gmail.com>
2024-03-12 21:49:18 -0700

    format raw data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './InputFields';
+import ram from './ram_on_plane.png';
+import InputFields from "./InputFields";
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header">
+            <h1>Jai Shri Ram Airlines</h1>
+        </header>
+        <main className="main">
+            <InputFields/>
+        </main>
+        <img srcSet={ram} alt={"ram on a ride"}/>
     </div>
-  );
-}
+);
 
 export default App;
